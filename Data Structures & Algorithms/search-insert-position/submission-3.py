ece0438000edbaa194333d4ae739ee68f2e9ef77
kwc0596 <54780612+kwc0596@users.noreply.github.com>
2026-05-21 12:12:46 -0700

    Add: search-insert-position - submission-3

---
+++ Data Structures & Algorithms/search-insert-position/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        #basically you set up the search insert position the exact same way
+        #we would a traditional binary search algorithm. Instead of returning -1,
+        #we will return the indice of the last value we are looking at.
+
+
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            midpoint = (l + r) // 2 
+            if nums[midpoint] < target: 
+                l = midpoint + 1
+            elif nums[midpoint] > target: 
+                r = midpoint -  1
+            elif nums[midpoint] == target: 
+                return midpoint
+        return l
