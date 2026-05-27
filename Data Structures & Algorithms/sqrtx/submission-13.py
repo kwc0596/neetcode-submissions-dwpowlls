e05ef84dd71c20047f2f1ea6a6212a543e5d82ee
kwc0596 <54780612+kwc0596@users.noreply.github.com>
2026-05-26 23:20:21 -0700

    Add: sqrtx - submission-13

---
+++ Data Structures & Algorithms/sqrtx/submission-13.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+
+        while l <= r: 
+            m = (l + r) // 2
+
+            if m * m < x: 
+                l = m + 1
+            elif m * m > x: 
+                r = m - 1
+            else: 
+                return m
+        return r
+
+        #we return r here because at this point if loop is no longer satisfied, l has been incremented out of bounds
